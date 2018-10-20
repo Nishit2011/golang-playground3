f6c18e7d269b06cce2694de0f76a9c1e9c4803be
Ranjan, Nishit (US - Bengaluru) <nishiranjan@deloitte.com>
2018-10-20 16:49:28 +0530

    Another way to declare  maps

--- main.go
+++ main.go
@@ -6,9 +6,13 @@ func main() {
 
 	//alternate way to declare maps
 	//var colors map[string]string
+
+	colors := make(map[string]string)
 	// colors := map[string]string{
 	// 	"red":   "#ff0000",
 	// 	"green": "4bf745",
 	// }
+
+	colors["white"] = "#ffffff"
 	fmt.Println(colors)
 }
